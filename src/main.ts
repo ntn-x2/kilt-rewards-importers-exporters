2f767b2228779824f0c273ae732ccdfd358c2761
weichweich <albrecht@kilt.io>
2022-09-27 07:22:02 +0200

    improve doc and config

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
-import dotenv from "dotenv"
-import { rpc } from "./imports"
-import { koinly } from "./exports"
-import { transformRpcInputToKoinlyOutput } from "./utils"
+import dotenv from 'dotenv'
+import { rpc } from './imports'
+import { koinly } from './exports'
+import { transformRpcInputToKoinlyOutput } from './utils'
 
-const octoberEventsHandler: rpc.ImportOptions["pageEventsHandler"] = async (events): Promise<void> => {
+const OUT_DIR = 'OUT_DIR'
+
+const octoberEventsHandler: rpc.ImportOptions['pageEventsHandler'] = async (
+    events
+): Promise<void> => {
     const transformedInput = transformRpcInputToKoinlyOutput(events)
-    return koinly.writeToCsv(transformedInput, process.env["OUT_PATH"])
+    return koinly.writeToCsv(transformedInput, process.env[OUT_DIR])
 }
 
 async function main() {
     dotenv.config()
 
     // Testing October retrieval
-    await rpc.retrieveAndFilterRewardEventData({ pageEventsHandler: octoberEventsHandler })
+    await rpc.retrieveAndFilterRewardEventData({
+        pageEventsHandler: octoberEventsHandler,
+    })
 }
 
 main()
